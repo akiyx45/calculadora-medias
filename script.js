f97112c08842d6d8ea608f3d9f3a74bc331535b0
Vinicius Esteves <brunaesteves02062gmail.com>
2026-03-30 21:54:43 -0300

    Mudança no script.

--- script.js
+++ script.js
@@ -12,8 +12,16 @@
         
         // --- SEU CÓDIGO COMEÇA AQUI ---
         
-        // Exemplo de como capturar:
-        // let n1 = parseFloat(document.getElementById('n1').value);
+        let n1 = parseFloat(document.getElementById('n1').value);
+        let n2 = parseFloat(document.getElementById('n2').value);
+        let n3 = parseFloat(document.getElementById('n3').value);
+        let n4 = parseFloat(document.getElementById('n4').value);
+
+        let media = (n1 + n2 + n3 + n4) / 4
+            outputDiv.innerHTML = '<strong.Sua média é:' + resultado.toFixed(2) + '</strong>';
+            
+
+
 
         // --- SEU CÓDIGO TERMINA AQUI ---
     }
